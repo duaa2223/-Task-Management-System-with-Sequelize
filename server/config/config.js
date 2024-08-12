701ae4ffc30e15c8bd7399b629c7f34029ab6e96
Dua'a Khamis <your_email@example.com>
2024-08-12 16:27:44 +0300

    add

---
+++ server/config/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "development": {
+    "username": "postgress",
+    "password": "12345",
+    "database": "test",
+    "host": "localhost",
+    "dialect": "postgress"
+  },
+ 
+}
